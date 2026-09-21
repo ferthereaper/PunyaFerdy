c8658bc647eb66bb1661e5f6d1c6ff0ea65cb2d6
ferthereaper <ferdyansahprakoso55@gmail.com>
2026-09-21 12:19:25 +0700

    Update tambah.php

--- api/anggota/tambah.php
+++ api/anggota/tambah.php
@@ -1,6 +1,9 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 $page_title = "Tambah Anggota";
-include __DIR__ . '/../includes/header.php';
+include __DIR__ . '/../../includes/header.php';
 
 $flash = $_SESSION['flash'] ?? null;
 unset($_SESSION['flash']);
@@ -34,4 +37,4 @@ unset($_SESSION['flash']);
                 </p>
             </form>
         </section>
-<?php include __DIR__ . '/../includes/footer.php'; ?>
+<?php include __DIR__ . '/../../includes/footer.php'; ?>
