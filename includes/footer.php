893a20450e2a4d165f94971fc56cd42237fa1856
ferthereaper <ferdyansahprakoso55@gmail.com>
2026-09-21 16:22:51 +0700

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 </main>
 
     <footer>
-        <p>&copy; <?php echo date('Y'); ?> Sistem Pemesanan UMKM &mdash; Jobsheet 7</p>
+        <p>&copy; <?php echo date('Y'); ?> Sistem Pemesanan UMKM &mdash; by Ferdy</p>
     </footer>
     <script src="/assets/js/app.js"></script>
     <?php if (!empty($extra_scripts)): foreach ($extra_scripts as $src): ?>
