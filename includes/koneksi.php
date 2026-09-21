c66c616a582ab2002dc50948e33dd0f0b0046898
ferthereaper <ferdyansahprakoso55@gmail.com>
2026-09-21 11:55:19 +0700

    Update koneksi.php

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -1,9 +1,9 @@
 <?php
-$host        = "aws-0-ap-southeast-1.pooler.supabase.com";
-$port        = "6543";
-$database    = "postgres";
-$user        = "postgres.rmvsvouottsiokychbok";
-$pass        = "@Ferdy270105";
+$host     = "aws-0-ap-southeast-1.pooler.supabase.com";
+$port     = "6543";
+$db       = "postgres";
+$user     = "postgres.rmvsvouottsiokychbok";
+$pass     = "@Ferdy270105";
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
